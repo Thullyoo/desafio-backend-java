b70349fe434b4a1f1b9c9f27c37d088f3639dfef
thullyo <thullyofilho@gmail.com>
2025-12-09 12:16:14 -0300

    feat: add ClienteService with method to register Cliente and AderirEmpresa

---
+++ src/main/java/br/thullyoo/desafio_backend_java/services/ClientService.java
@@ -0,0 +1,59 @@
+package br.thullyoo.desafio_backend_java.services;
+
+import br.thullyoo.desafio_backend_java.dto.ClientRequest;
+import br.thullyoo.desafio_backend_java.model.Cliente;
+import br.thullyoo.desafio_backend_java.model.Empresa;
+import br.thullyoo.desafio_backend_java.repositories.ClienteRepository;
+import br.thullyoo.desafio_backend_java.repositories.EmpresaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    public final ClienteRepository clienteRepository;
+
+    public final EmpresaRepository empresaRepository;
+
+    public ClientService(ClienteRepository clienteRepository, EmpresaRepository empresaRepository) {
+        this.clienteRepository = clienteRepository;
+        this.empresaRepository = empresaRepository;
+    }
+
+    public Cliente register(ClientRequest clientRequest){
+        Cliente cliente = new Cliente();
+        cliente.setCpf(clientRequest.cpf());
+        cliente.setNome(clientRequest.nome());
+        cliente.setEmail(clientRequest.email());
+
+        String cnpj = clientRequest.empresa_cpnj();
+
+        if (cnpj != null && !cnpj.isBlank()) {
+            Optional<Empresa> empresa = empresaRepository.findById(cnpj);
+            if (empresa.isEmpty()) {
+                throw new RuntimeException("Empresa não encontrada");
+            }
+            cliente.setEmpresa(empresa.get());
+        }
+
+        clienteRepository.save(cliente);
+        return cliente;
+    }
+
+    public Cliente aderirEmpresa(String cpf, String cnpj){
+        Optional<Cliente> cliente =  clienteRepository.findByCpf(cpf);
+        if (cliente.isEmpty()){
+            throw new RuntimeException("Cliente não encontrado");
+        }
+        if (cliente.get().getEmpresa() != null){
+            Optional<Empresa> empresa = empresaRepository.findByCnpj(cnpj);
+            if (empresa.isEmpty()){
+                throw new RuntimeException("Empresa não encontrada");
+            }
+            cliente.get().setEmpresa(empresa.get());
+        }
+        clienteRepository.save(cliente.get());
+        return cliente.get();
+    }
+}
