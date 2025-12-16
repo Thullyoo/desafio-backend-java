a4f7b6732b4c62cc81bc75f8520633c7bb96ce04
thullyo <thullyofilho@gmail.com>
2025-12-16 11:40:34 -0300

    feat: add contatos on ClienteResponse

--- desafio-2/src/main/java/br/thullyoo/desafio_2/dto/ClienteResponse.java
+++ desafio-2/src/main/java/br/thullyoo/desafio_2/dto/ClienteResponse.java
@@ -1,7 +1,10 @@
 package br.thullyoo.desafio_2.dto;
 
+import java.util.List;
+
 public record ClienteResponse(
         String name,
-        String email
+        String email,
+        List<ContatoResponse> contatos
 ) {
 }
