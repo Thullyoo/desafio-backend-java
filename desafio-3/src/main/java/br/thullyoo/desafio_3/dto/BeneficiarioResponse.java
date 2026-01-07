fc38d848045fc61b39c6f800e396c02936004c20
thullyo <thullyofilho@gmail.com>
2026-01-07 12:49:17 -0300

    fix: adjusting parameters

--- desafio-3/src/main/java/br/thullyoo/desafio_3/dto/BeneficiarioResponse.java
+++ desafio-3/src/main/java/br/thullyoo/desafio_3/dto/BeneficiarioResponse.java
@@ -1,8 +1,17 @@
 package br.thullyoo.desafio_3.dto;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public record BeneficiarioResponse(UUID id, String nome, String telefone, LocalDate dataNascimento, LocalDate dataInclusao, LocalDate dataAtualizacao, List<DocumentoResponse> documentos) {
+public record BeneficiarioResponse(
+        UUID id,
+        String nome,
+        String telefone,
+        LocalDate dataNascimento,
+        LocalDateTime dataInclusao,
+        LocalDateTime dataAtualizacao,
+        List<DocumentoResponse> documentos
+) {
 }
--- desafio-3/src/main/java/br/thullyoo/desafio_3/dto/DocumentoResponse.java
+++ desafio-3/src/main/java/br/thullyoo/desafio_3/dto/DocumentoResponse.java
@@ -1,13 +1,14 @@
 package br.thullyoo.desafio_3.dto;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record DocumentoResponse(
         UUID id,
         String tipoDocumento,
         String descricao,
-        LocalDate dataInclusao,
-        LocalDate dataAtualizacao
+        LocalDateTime dataInclusao,
+        LocalDateTime dataAtualizacao
 ) {
 }
