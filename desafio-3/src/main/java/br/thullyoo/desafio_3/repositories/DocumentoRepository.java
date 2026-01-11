a10c3672258e9e5ffbfe185e8e269b01c91f3f20
thullyo <thullyofilho@gmail.com>
2026-01-11 14:42:56 -0300

    feat: add method to get Documentos by Beneficiaro ID

--- desafio-3/src/main/java/br/thullyoo/desafio_3/repositories/DocumentoRepository.java
+++ desafio-3/src/main/java/br/thullyoo/desafio_3/repositories/DocumentoRepository.java
@@ -1,11 +1,14 @@
 package br.thullyoo.desafio_3.repositories;
 
+import br.thullyoo.desafio_3.model.Beneficiario;
 import br.thullyoo.desafio_3.model.Documento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface DocumentoRepository extends JpaRepository<Documento, UUID> {
+    List<Documento> findByBeneficiario(Beneficiario beneficiario);
 }
