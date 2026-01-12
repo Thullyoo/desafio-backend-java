a46a90877e28c4fb316a98a24f971049ec7ee753
thullyo <thullyofilho@gmail.com>
2026-01-12 12:59:12 -0300

    feat: add endpoint to list Documentos by Beneficiario Id and update Beneficiario

--- desafio-3/src/main/java/br/thullyoo/desafio_3/controller/BeneficiarioController.java
+++ desafio-3/src/main/java/br/thullyoo/desafio_3/controller/BeneficiarioController.java
@@ -3,12 +3,14 @@ package br.thullyoo.desafio_3.controller;
 import br.thullyoo.desafio_3.dto.BeneficiarioListResponse;
 import br.thullyoo.desafio_3.dto.BeneficiarioRequest;
 import br.thullyoo.desafio_3.dto.BeneficiarioResponse;
+import br.thullyoo.desafio_3.dto.DocumentoResponse;
 import br.thullyoo.desafio_3.service.BeneficiarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController()
 @RequestMapping("/api/beneficiarios")
@@ -25,5 +27,15 @@ public class BeneficiarioController {
     @GetMapping
     public ResponseEntity<List<BeneficiarioListResponse>> listar(){
         return ResponseEntity.ok(beneficiarioService.listar());
+    }
+
+    @GetMapping("/{beneficiarioId}")
+    public ResponseEntity<List<DocumentoResponse>> listarDocumentos(@PathVariable("beneficiarioId")UUID beneficiarioId){
+        return ResponseEntity.ok(beneficiarioService.listarDocumetosPorId(beneficiarioId));
+    }
+
+    @PutMapping("/{beneficiarioId}")
+    public ResponseEntity<BeneficiarioListResponse> atualizar(@PathVariable("beneficiarioId")UUID beneficiarioId, @RequestBody BeneficiarioRequest beneficiarioRequest){
+        return ResponseEntity.ok(beneficiarioService.atualizar(beneficiarioId, beneficiarioRequest));
     }
 }
